ffb7b038a8691e07e9a3a9f1890d8be9a6f4bd45
Rich Chang <rich_chang@outlook.com>
2020-05-26 11:38:54 +0800

    Newly add empty activity for setting

---
+++ app/src/main/java/com/aivie/aivie/user/presentation/setting/SettingActivity.java
@@ -0,0 +1,16 @@
+package com.aivie.aivie.user.presentation.setting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.aivie.aivie.user.R;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
