cf2b8f4f5d08b56d1ab58ea2b1ad9153d702e21a
Rich Chang <rich_chang@outlook.com>
2020-05-26 12:52:07 +0800

    Implement button click to logout

--- app/src/main/java/com/aivie/aivie/user/presentation/setting/SettingActivity.java
+++ app/src/main/java/com/aivie/aivie/user/presentation/setting/SettingActivity.java
@@ -2,9 +2,13 @@ package com.aivie.aivie.user.presentation.setting;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.aivie.aivie.user.R;
+import com.aivie.aivie.user.presentation.account.LoginActivity;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SettingActivity extends AppCompatActivity {
 
@@ -12,5 +16,13 @@ public class SettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+    }
+
+    public void clickLogout(View view) {
+
+        FirebaseAuth.getInstance().signOut();
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
