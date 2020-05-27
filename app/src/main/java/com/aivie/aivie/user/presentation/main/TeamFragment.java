a3a2e9ca77e82d8becbe2d499b4caf0b224ddcf2
Rich Chang <rich_chang@outlook.com>
2020-05-27 17:50:15 +0800

    Implement basic site info in TeamView
    Get data from userProfile SP file

--- app/src/main/java/com/aivie/aivie/user/presentation/main/TeamFragment.java
+++ app/src/main/java/com/aivie/aivie/user/presentation/main/TeamFragment.java
@@ -4,12 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.aivie.aivie.user.R;
+import com.aivie.aivie.user.data.user.UserProfileSpImpl;
+
+import org.w3c.dom.Text;
+
+import java.util.Objects;
 
 public class TeamFragment extends Fragment {
 
@@ -20,6 +26,18 @@ public class TeamFragment extends Fragment {
         //return super.onCreateView(inflater, container, savedInstanceState);
         View root = inflater.inflate(R.layout.fragment_team, container, false);
 
+        displayTeamInfo(root);
+
         return root;
+    }
+
+    private void displayTeamInfo(View view) {
+
+        UserProfileSpImpl userProfileSp = new UserProfileSpImpl(Objects.requireNonNull(getActivity()));
+
+        ((TextView) view.findViewById(R.id.textViewSiteId)).setText(userProfileSp.getSiteId());
+        ((TextView) view.findViewById(R.id.textViewSiteDoctor)).setText(userProfileSp.getSiteDoctor());
+        ((TextView) view.findViewById(R.id.textViewSiteSC)).setText(userProfileSp.getSiteSC());
+        ((TextView) view.findViewById(R.id.textViewSitePhone)).setText(userProfileSp.getSitePhone());
     }
 }
