beb308b5483abad848cff00179e09e8c3e8fbffd
Rich Chang <rich_chang@outlook.com>
2020-05-28 23:24:58 +0800

    Create empty activity for adverse event view
    -with no action bar

---
+++ app/src/main/java/com/aivie/aivie/user/presentation/therapy/AdverseEventActivity.java
@@ -0,0 +1,16 @@
+package com.aivie.aivie.user.presentation.therapy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.aivie.aivie.user.R;
+
+public class AdverseEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adverse_event);
+    }
+}
