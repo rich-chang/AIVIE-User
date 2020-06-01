442182cdd5fe27bec45f8877fcf332d410f77574
Rich Chang <rich_chang@outlook.com>
2020-06-01 23:14:58 +0800

    Implement calender picker for event reported date

--- app/src/main/java/com/aivie/aivie/user/presentation/therapy/AdverseEventDialog.java
+++ app/src/main/java/com/aivie/aivie/user/presentation/therapy/AdverseEventDialog.java
@@ -1,5 +1,6 @@
 package com.aivie.aivie.user.presentation.therapy;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -7,6 +8,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
@@ -14,8 +17,15 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
 import com.aivie.aivie.user.R;
+import com.aivie.aivie.user.data.Constant;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class AdverseEventDialog extends DialogFragment implements View.OnClickListener{
+
+    private final Calendar myCalendar = Calendar.getInstance();
 
     public AdverseEventDialog() {
 
@@ -35,6 +45,7 @@ public class AdverseEventDialog extends DialogFragment implements View.OnClickLi
 
         initActionButton(view);
         initAdverseEvents(view);
+        initEventReportedDate(view);
 
         return view;
     }
@@ -68,6 +79,36 @@ public class AdverseEventDialog extends DialogFragment implements View.OnClickLi
 
         Spinner adverseEvents = view.findViewById(R.id.spinnerAdverseEvent);
         adverseEvents.setAdapter(nAdapter);
+    }
+
+    private void initEventReportedDate(View view) {
+
+        final EditText editTextReportedDate = view.findViewById(R.id.editTextReportDate);
+
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                SimpleDateFormat sdf = new SimpleDateFormat(Constant.DATE_FORMAT_SIMPLE, Locale.US);
+                editTextReportedDate.setText(sdf.format(myCalendar.getTime()));
+            }
+        };
+
+        editTextReportedDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(v.getContext(),
+                        date,
+                        myCalendar.get(Calendar.YEAR),
+                        myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
     }
 
     private void saveDataToSp() {
