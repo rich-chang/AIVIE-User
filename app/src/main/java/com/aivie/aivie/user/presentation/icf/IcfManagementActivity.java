32550f052906a31e378a784848262e75a14f6a49
Rich Chang <rich-chang@outlook.com>
2020-06-11 17:53:48 +0800

    Init ICF management activity

---
+++ app/src/main/java/com/aivie/aivie/user/presentation/icf/IcfManagementActivity.java
@@ -0,0 +1,16 @@
+package com.aivie.aivie.user.presentation.icf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.aivie.aivie.user.R;
+
+public class IcfManagementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_icf_management);
+    }
+}
