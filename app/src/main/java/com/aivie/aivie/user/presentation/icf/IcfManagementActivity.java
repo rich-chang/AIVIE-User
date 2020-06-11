dfaf81babcd87038e8254dec2d73d6c1384e0f3a
Rich Chang <rich-chang@outlook.com>
2020-06-11 18:10:04 +0800

    Set action bar title

--- app/src/main/java/com/aivie/aivie/user/presentation/icf/IcfManagementActivity.java
+++ app/src/main/java/com/aivie/aivie/user/presentation/icf/IcfManagementActivity.java
@@ -12,5 +12,7 @@ public class IcfManagementActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_icf_management);
+
+        getSupportActionBar().setTitle("eICF Document History");
     }
 }
