5638346fa4bd636ec41e900f526ebfbae23d908d
Rich <rich_chang@outlook.com>
2020-07-31 00:47:20 +0800

    Set sample x ray image for demo

--- app/src/main/java/com/aivie/aivie/user/presentation/therapy/PatientReportActivity.java
+++ app/src/main/java/com/aivie/aivie/user/presentation/therapy/PatientReportActivity.java
@@ -2,15 +2,35 @@ package com.aivie.aivie.user.presentation.therapy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.aivie.aivie.user.R;
 
-public class PatientReportActivity extends AppCompatActivity {
+public class PatientReportActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_report);
+
+        findViewById(R.id.ll_LabReport).setOnClickListener(this);
+        findViewById(R.id.ll_ImageReport).setOnClickListener(this);
+        findViewById(R.id.ll_ECG).setOnClickListener(this);
+        findViewById(R.id.ll_ePRO).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.ll_LabReport:
+            case R.id.ll_ImageReport:
+            case R.id.ll_ECG:
+            case R.id.ll_ePRO:
+                startActivity(new Intent(this, ImageGalleryActivity.class));
+                break;
+        }
     }
 }
